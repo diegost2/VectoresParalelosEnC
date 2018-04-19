f2bba5b61472e64f5d49eeb3a33ddecad09ddce3
alumno <alumno>
2018-04-19 12:09:30 -0300

    Segundo commit

--- Vectores Paralelos/main.c
+++ Vectores Paralelos/main.c
@@ -4,6 +4,7 @@
 
 void mostrarAlumno(char [][50], int [], int [], float [], int [], int );
 void CargarAlumno(char [][50], int [], int [], float [], int [], int );
+int BuscarLugar(int [],int );
 
 int main()
 {
@@ -26,12 +27,26 @@ void mostrarAlumno(char nombre[][50], int notaUno[], int notaDos[], float promed
         printf("\nLegajo: %d , Nombre: %s , Nota1: %d , Nota 2: %d , Promedio: %.2f ",legajo[i],nombre[i],notaUno[i],notaDos[i],promedio[i]);
 
     }
-
 }
 
 void CargarAlumno(char nombre[][50], int notaUno[], int notaDos[], float promedio[], int legajo[], int largo){
+    int indice;
+    legajo[indice]=666;
+    indice=BuscarLugar(legajo,largo);
+
+
+}
+
+int BuscarLugar(int legajo[],int largo){
     int i;
-    legajo[1]=666;
-
-
+    int indiceRetornado=-1;
+    for(i=0;i<largo;i++)
+    {
+        if(legajo[i]==0)
+        {
+            indiceRetornado=i;
+        }
+        break;
+    }
+    return indiceRetornado;
 }
